178c7874aa6c56d2d90610677a61483ba37c206d
Yogita Shete <yogitashete24@gmail.com>
2023-12-07 15:49:24 +0530

    feat : create add product component

--- client/src/views/AddProduct/AddProduct.js
+++ client/src/views/AddProduct/AddProduct.js
@@ -1,13 +1,92 @@
-import React from 'react'
-import Navbar from '../../components/Navbar/Navbar'
+import React, { useEffect, useState } from "react";
+import Navbar from "../../components/Navbar/Navbar";
 
 function AddProduct() {
+  const [productName, setProductName] = useState("");
+  const [price, setPrice] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [description, setDescription] = useState("");
+
+  // const addProducts=()=>{
+  
+  // } ;
+
+  // useEffect(()=>{
+  //   addProducts();
+  // },[]);
+
   return (
     <>
-    <Navbar/>
-<h1 className='text-center'>Add Your Products</h1>  
-  </>
-  )
+      <Navbar />
+      <div>
+      <form className="form-control mx-auto " action="">
+        <p className="title">Add Your Products</p>
+        <div className="input-field">
+          <input
+            required=""
+            className="input"
+            type="text"
+            value={productName}
+            onChange={(e) => {
+              setProductName(e.target.value);
+            }}
+          />
+          <label className="label" for="input">
+            Enter Product Name    
+          </label>
+        </div>
+
+        <div className="input-field">
+          <input
+            required=""
+            className="input"
+            type="text"
+            value={price}
+            onChange={(e) => {
+              setPrice(e.target.value);
+            }}
+          />
+          <label className="label" for="input">
+            Enter Price
+          </label>
+        </div>
+        <div className="input-field">
+          <input
+            required=""
+            className="input"
+            type="text"
+            value={quantity}
+            onChange={(e) => {
+              setQuantity(e.target.value);
+            }}
+          />
+          <label className="label" for="input">
+            Enter Quantity
+          </label>
+        </div>
+        <div className="input-field">
+        <textarea
+          name="description"
+          value={description}
+          onChange={(e)=>{
+            setDescription(e.target.value);
+          }}
+          placeholder="Description"
+          className="w-full p-2 border rounded"
+        ></textarea>
+        </div>
+
+        <button type="button" className="submit-btn bg-red-600"
+        //  onClick={addProducts}
+         >
+          Add Products
+        </button>
+      </form>
+
+      </div>
+
+    </>
+  );
 }
 
-export default AddProduct
+export default AddProduct;
